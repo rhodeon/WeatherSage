40a6959c06907f70112da075be97cf2206932ddd
rhodeon <ruonabraks@gmail.com>
2020-09-02 15:25:00 +0100

    Added progress bar to weekly forecast

--- app/src/main/java/com/rhodeon/weathersage/forecast/WeeklyForecastFragment.kt
+++ app/src/main/java/com/rhodeon/weathersage/forecast/WeeklyForecastFragment.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.rhodeon.weathersage.*
 import com.rhodeon.weathersage.api.DailyForecast
 import com.rhodeon.weathersage.api.WeeklyForecast
@@ -46,6 +46,7 @@ class WeeklyForecastFragment : Fragment() {
         binding.forecastView.adapter = dailyForecastAdapter
 
         val viewModelObserver = Observer<WeeklyForecast> { viewState->
+            binding.weeklyForecastProgress.isGone = true    // remove progress bar on loaded state
             dailyForecastAdapter.submitList(viewState.daily)
         }
         viewModel.viewState.observe(viewLifecycleOwner, viewModelObserver)
@@ -68,7 +69,10 @@ class WeeklyForecastFragment : Fragment() {
         locationRepository = LocationRepository(requireContext())
         val savedLocationObserver = Observer<Location> {savedLocation ->
             when (savedLocation) {
-                is Location.Zipcode -> viewModel.loadWeeklyForecasts(savedLocation.zipcode)
+                is Location.Zipcode -> {
+                    binding.weeklyForecastProgress.isVisible = true     // display progress bar while loading state
+                    viewModel.loadWeeklyForecasts(savedLocation.zipcode)    // load weather data
+                }
             }
         }
         locationRepository.savedLocation.observe(viewLifecycleOwner, savedLocationObserver)
